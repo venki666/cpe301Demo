409d2e430936e43af4b8c38d7736c86e83fe0812
Venki Muthukumar <vm@unlv.nevada.edu>
2019-04-03 00:01:12 -0700

    SSD1306-oled-I2C Commit

---
+++ Libraries/SSD1306-oled-I2C/SSD1306-oled-I2C/main.c
@@ -0,0 +1,20 @@
+//****main.c****//
+#define F_CPU 16000000UL
+#include "lcd.h"
+
+
+int main(void){
+  lcd_init(LCD_DISP_ON);    // init lcd and turn on
+  
+  lcd_puts("Hello World");  // put string from RAM to display (TEXTMODE) or buffer (GRAPHICMODE)
+  lcd_gotoxy(0,2);          // set cursor to first column at line 3
+  lcd_puts_p(PSTR("String from flash"));  // puts string form flash to display (TEXTMODE) or buffer (GRAPHICMODE)
+#if defined GRAPHICMODE
+  lcd_drawCircle(64,32,7,WHITE); // draw circle to buffer white lines
+  lcd_display();                 // send buffer to display
+#endif
+  for(;;){
+    //main loop
+  }
+  return 0;
+}
