1c06dfd4df3cf8d2c1d0d6a183118f57e377688b
venki666 <vm@unlv.nevada.edu>
2021-06-30 10:01:26 -0700

    Add files via upload

---
+++ OnlineDemos/Online_Demo_Cprogram/ATmega_UltrasonicHCSR05/main.c
@@ -0,0 +1,100 @@
+/*
+ * ATmega_UltrasonicHCSR05.c
+ *
+ * Created: 6/29/2021 11:48:54 PM
+ * Author : VenkatesanMuthukumar
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <string.h>
+
+#define BAUDRATE 9600
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
+char buffer[5];                //Output of the itoa function
+
+void USART_init(void){
+	
+	UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
+	UBRR0L = (uint8_t)(BAUD_PRESCALLER);
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UCSR0C = (3<<UCSZ00);
+}
+
+void USART_send( unsigned char data){
+	
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
+	
+}
+
+void USART_putstring(char* StringPtr){
+	
+	while(*StringPtr != 0x00){
+		USART_send(*StringPtr);
+	StringPtr++;}
+	
+}
+
+#define  Trigger_pin	PB0	/* Trigger pin */
+
+int TimerOverflow = 0;
+
+ISR(TIMER1_OVF_vect)
+{
+	TimerOverflow++;		/* Increment Timer Overflow count */
+}
+
+int main(void)
+{
+	char string[10];
+	long count;
+	double distance;
+	
+	DDRB = 0x01;			/* Make trigger pin as output */
+	
+	USART_init();
+	
+	sei();					/* Enable global interrupt */
+	TIMSK1 = (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
+	TCCR1A = 0;				/* Set all bit to zero Normal operation */
+
+	while(1)
+	{
+
+		PORTB |= (1 << Trigger_pin);/* Give 10us trigger pulse on trig. pin to HC-SR04 */
+		_delay_us(10);
+		PORTB &= (~(1 << Trigger_pin));
+		
+		TCNT1 = 0;			/* Clear Timer counter */
+		TCCR1B = 0x41;		/* Setting for capture rising edge, No pre-scaler*/
+		TIFR1 = 1<<ICF1;		/* Clear ICP flag (Input Capture flag) */
+		TIFR1 = 1<<TOV1;		/* Clear Timer Overflow flag */
+
+		/*Calculate width of Echo by Input Capture (ICP) on PortD PD6 */
+		
+		while ((TIFR1 & (1 << ICF1)) == 0);	/* Wait for rising edge */
+		TCNT1 = 0;			/* Clear Timer counter */
+		TCCR1B = 0x01;		/* Setting for capture falling edge, No pre-scaler */
+		TIFR1 = 1<<ICF1;		/* Clear ICP flag (Input Capture flag) */
+		TIFR1 = 1<<TOV1;		/* Clear Timer Overflow flag */
+		TimerOverflow = 0;	/* Clear Timer overflow count */
+
+		while ((TIFR1 & (1 << ICF1)) == 0); /* Wait for falling edge */
+		count = ICR1 + (65535 * TimerOverflow);	/* Take value of capture register */
+		/* 8MHz Timer freq, sound speed =343 m/s, calculation mentioned in doc. */
+		distance = (double)count / 466.47;
+
+		dtostrf(distance, 2, 2, string);/* Convert distance into string */
+		strcat(string, " cm   ");
+		USART_putstring("Dist = ");
+		USART_putstring(string);	/* Print distance on Terminal */
+		_delay_ms(200);
+	}
+}
+
